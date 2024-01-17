7352a9b1232a10f18bcd64488d347b08e5c6bdc8
Thomas W Rackers <twrackers@gmail.com>
2024-01-16 22:24:22 -0500

    New collection of FastClock sketches for Arduino.

---
+++ FastClock/FastClock.ino
@@ -0,0 +1,33 @@
+#include <StateMachine.h>
+
+//#include "SparkFun_Alphanumeric.h"
+#include "SparkFun_OLED.h"
+
+#define FAST 12
+#define INTERVAL (60000L / FAST)
+
+StateMachine timer(INTERVAL, true);
+
+Display display;
+
+int hr = 0;
+int mn = 0;
+
+void setup() {
+
+  display.begin();
+  display.showTime(hr, mn);
+  
+}
+
+void loop() {
+  if (timer.update()) {
+    if (++mn == 60) {
+      mn = 0;
+      if (++hr == 24) {
+        hr = 0;
+      }
+    }
+    display.showTime(hr, mn);
+  }
+}
